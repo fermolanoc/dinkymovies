1bec59e465c2308761738c1417ee07b277037461
Fernando Molano <fm87usa@gmail.com>
2021-01-26 21:39:55 -0600

    add searchable area, get data from API

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+// Initial Values
+const API_KEY = '50e10f8fa5798672541651fdfd8b637b';
+
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY;
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+// Selecting elements from the DOM
+const buttonElement = document.querySelector('#search');
+const inputElement = document.querySelector('#inputValue');
+const movieSearchable = document.querySelector('#movies-searchable');
+
+
+
+function movieSection(movies) {
+    return movies.map((movie) => {
+        return `
+        <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>>
+        `
+    })
+}
+
+function createMovieContainer(movies) {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const movieTemplate = `
+    <section class="section">
+    ${movieSection(movies)}
+    </section>
+    <div class="content">
+      <p id="content-close">X</p>
+    </div>
+    `;
+
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
+}
+
+buttonElement.onclick = function (event) {
+    event.preventDefault();
+    const value = inputElement.value;
+    const newUrl = url + '&query=' + value;
+
+    fetch(newUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            // data results
+            const movies = data.results;
+            const movieBlock = createMovieContainer(movies);
+            movieSearchable.appendChild(movieBlock)
+
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        })
+}
